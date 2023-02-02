16132a0ad417ad4232a7314cd6bdb7551f27456c
paddy.downey <G00392495@atu.ie>
2023-02-02 13:00:44 +0000

    added TV

---
+++ src/main/java/ie/atu/productv4/TV.java
@@ -0,0 +1,34 @@
+package ie.atu.productv4;
+
+public class TV extends Product {
+
+    private String manufacture;
+    private String screen;
+
+    public TV() {
+        super();
+        manufacture = "";
+        screen = "";
+        count++;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+    public void setScreen(String screen) {
+        this.screen = screen;
+    }
+
+    public String getScreen() {
+        return screen;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + manufacture + " \nscreen size is " + screen;
+    }
+}
